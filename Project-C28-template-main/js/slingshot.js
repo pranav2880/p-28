a15e5ff21def521cff87c8536acb539b90ff6a09
pranav2880 <74184559+pranav2880@users.noreply.github.com>
2021-05-25 15:06:45 +0530

    Add files via upload

---
+++ Project-C28-template-main/js/slingshot.js
@@ -0,0 +1,28 @@
+class Slingshot{
+    constructor(bodyA,pointB) {
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.1,
+            length:100
+        }
+        this.pointB=pointB;
+        this.chain=Constraint.create(options);
+        World.add(world,this.chain);
+    }  
+    display(){
+    if(this.chain.bodyA){
+        var pa=this.chain.bodyA.position
+    var pb=this.pointB;
+    strokeWeight(4);
+        line(pa.x,pa.y,pb.x,pb.y)
+    }
+}
+fly(){
+    this.chain.bodyA=null;
+    
+}
+attach(body){
+    this.chain.bodyA = body
+}
+}
